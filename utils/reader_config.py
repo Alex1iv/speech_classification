678c713984a0f5944b24ec7ec17d7169be6acade
Alex1Iv <ivanov.politehnik@gmail.com>
2025-02-25 00:32:46 +0600

    fix loading useless attributes

--- utils/reader_config.py
+++ utils/reader_config.py
@@ -1,20 +1,16 @@
 import json
 from dotmap import DotMap
 
-def config_reader(path_to_json_conf: str) -> dict:
+def config_reader(path_to_json_conf: str) -> 'dotmap.DotMap':
     """Load parameters from configuration file.
 
     Args:
-    ------------
-    path_to_json_conf (_str_): путь к файлу конфигурации
+        path_to_json_conf (str): path to the configuration file
 
     Returns:
-    ------------
-    config (dict): dictionary with parameters
-    """    
+        DotMap: variable with parameters
+    """   
     with open(path_to_json_conf, 'r') as config_file:
         config_dict = json.load(config_file)
-    
-    config = DotMap(config_dict)
-    
-    return config
+ 
+    return DotMap(config_dict, _dynamic=False)
