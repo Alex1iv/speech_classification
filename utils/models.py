891163a0735accfa850277ec0a6e71a2511a6eae
Alex1Iv <ivanov.politehnik@gmail.com>
2025-03-08 01:27:19 +0600

    rewised model structure

--- utils/models.py
+++ utils/models.py
@@ -3,49 +3,7 @@ import os
 from tensorflow import keras
 import tensorflow as tf
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
-from tensorflow.keras import layers
-
-
-class ModelVoice(tf.keras.Model):
-    def __init__(self, data, output_units=30, **kwargs):#mean, variance
-        super(ModelVoice, self).__init__( **kwargs)
-        #self.mean = mean
-        #self.variance = variance
-        self.output_units = output_units
-        self.d1 = layers.Resizing(32, 32, name='Resizing')
-        self.d2 = layers.Normalization(
-            #mean=self.mean, 
-            #variance=self.variance,
-            name='Normalization')
-        self.d2.adapt(data=data.map(map_func=lambda spec, label: spec))  # for tensorflow
-        #self.d2.adapt(data=data)                                        # for NumPy
-        self.d3 = layers.Conv2D(32, 3, activation='relu', name='Convol_1')
-        self.d4 = layers.Conv2D(64, 3, activation='relu', name='Convol_2')
-        self.d5 = layers.MaxPooling2D(name='MaxPooling2D')
-        self.d6 = layers.Dropout(0.25, name='Dropout_0.25')
-        self.d7 = layers.Flatten(name='Flatten')
-        self.d8 = layers.Dense(128, activation='relu', name='Dense_128')
-        self.d9 = layers.Dropout(0.5, name='Dropout_0.5')
-        self.output_channel = layers.Dense(units=self.output_units, name='Dense_31')
-
-    def call(self, inputs, training=False): #, training=False
-        x = self.d1(inputs)
-        x = self.d2(x)
-        x = self.d3(x) 
-        x = self.d4(x) 
-        x = self.d5(x) 
-        x = self.d6(x)
-        #x = self.Dropout_1(x, training=training)
-        x = self.d7(x) 
-        x = self.d8(x) 
-        x = self.d9(x)
-        x = self.output_channel(x)
-        #print('Output: ', x.shape)
-        return x
-        #print(f"input_shape = {(self.n_timesteps, self.n_channels)} | output_units = {self.output_channels.shape}")
-    
-    # def get_model():
-    #     return ModelVoice(name='ModelVoice')
+from tensorflow.keras import layers, models
 
 def callbacks(model_name:str, path_models:str,
     reduce_patience:int,
@@ -115,6 +73,141 @@ def callbacks(model_name:str, path_models:str,
     # )
 
     return [checkpoint, earlystop, reduce_lr] 
+
+
+class ModelVoice_v0(tf.keras.Model):
+    def __init__(self, data, output_units=30, **kwargs):
+        super(ModelVoice_v0, self).__init__( **kwargs)
+
+        # Feature extraction
+        self.feature_extractor = models.Sequential([
+            layers.Resizing(32, 32, name='Resizing'),
+            layers.Conv2D(32, (3, 3), activation='relu', padding="same", name='Conv2D_1'),
+            layers.Conv2D(64, (3, 3), activation='relu', padding="same", name='Conv2D_2'),
+            layers.MaxPooling2D(pool_size=(2, 2), name='MaxPool2D_1'),
+            layers.Dropout(0.25, name='Dropout_1')
+        ], name="FeatureExtractor")
+
+        # Flattening & Fully Connected Layers
+        self.classifier = models.Sequential([
+            layers.Flatten(name='Flatten'),
+            layers.Dense(128, activation='relu', name='Dense_128'),
+            layers.Dropout(0.5, name='Dropout_2'),
+            layers.Dense(output_units, name='Output')
+        ], name="Classifier")
+
+        # Normalization layer adapted to dataset
+        self.norm_layer = layers.Normalization(name='Normalization')
+        self.norm_layer.adapt(data.map(lambda spec, label: spec))
+
+    def call(self, inputs, training=False):
+        x = self.norm_layer(inputs)
+        x = self.feature_extractor(x, training=training)
+        x = self.classifier(x, training=training)
+        return x
+    #     self.output_units = output_units
+    #     self.d1 = layers.Resizing(32, 32, name='Resizing')
+    #     self.d2 = layers.Normalization(
+    #         #mean=self.mean, 
+    #         #variance=self.variance,
+    #         name='Normalization')
+    #     self.d2.adapt(data=data.map(map_func=lambda spec, label: spec))  # for tensorflow
+    #     #self.d2.adapt(data=data)                                        # for NumPy
+    #     self.d3 = layers.Conv2D(32, 3, activation='relu', name='Convol_1')
+    #     self.d4 = layers.Conv2D(64, 3, activation='relu', name='Convol_2')
+    #     self.d5 = layers.MaxPooling2D(name='MaxPooling2D')
+    #     self.d6 = layers.Dropout(0.25, name='Dropout_0.25')
+    #     self.d7 = layers.Flatten(name='Flatten')
+    #     self.d8 = layers.Dense(128, activation='relu', name='Dense_128')
+    #     self.d9 = layers.Dropout(0.5, name='Dropout_0.5')
+    #     self.output_channel = layers.Dense(units=self.output_units, name='Dense_31')
+
+    # def call(self, inputs, training=False): #, training=False
+    #     x = self.d1(inputs)
+    #     x = self.d2(x)
+    #     x = self.d3(x) 
+    #     x = self.d4(x) 
+    #     x = self.d5(x) 
+    #     x = self.d6(x)
+    #     #x = self.Dropout_1(x, training=training)
+    #     x = self.d7(x) 
+    #     x = self.d8(x) 
+    #     x = self.d9(x)
+    #     x = self.output_channel(x)
+        #print('Output: ', x.shape)
+        # return x
+        #print(f"input_shape = {(self.n_timesteps, self.n_channels)} | output_units = {self.output_channels.shape}")
+    
+    # def get_model():
+    #     return ModelVoice(name='ModelVoice')
+
+def normalize_dataset(dataset):
+    """Compute mean & variance and normalize the dataset."""
+    # Extract features only (discard labels)
+    data_only = dataset.map(lambda spec, label: spec)
+
+    # Compute mean & variance efficiently
+    data_tensor = tf.concat(list(data_only), axis=0)
+    mean, var = tf.nn.moments(data_tensor, axes=[0])
+
+    # apply normalization
+    return dataset.map(lambda spec, label: ((spec - mean) / tf.sqrt(var), label))
+
+class ModelVoice_v1(tf.keras.Model):
+    def __init__(self, output_units:int=31, **kwargs):
+        super(ModelVoice_v1, self).__init__(**kwargs)
+        
+        self.layers_list = [
+            layers.Conv2D(32, (7, 3), activation='relu', padding="same", name='Conv2D_1'),
+            layers.MaxPooling2D(pool_size=(1, 3), name='MaxPool2D_1'),
+            layers.Conv2D(64, (1, 7), activation='relu', padding="same", name='Conv2D_2'),
+            layers.MaxPooling2D(pool_size=(1, 4), name='MaxPool2D_2'),
+            layers.Dropout(0.25, name='Dropout_1'),
+            layers.Conv2D(128, (1, 10), activation='relu', padding="valid", name='Conv2D_3'),
+            layers.Conv2D(256, (7, 1), activation='relu', padding="valid", name='Conv2D_4'),
+            layers.GlobalMaxPooling2D(),
+            layers.Dropout(0.5, name='Dropout_2'),
+            layers.Dense(128, name='Dense_1'),
+            layers.Dense(output_units, name='Output')]
+
+    def call(self, inputs, training=False):
+        x = inputs
+        #x = self.input_layer(inputs)
+        for layer in self.layers_list:
+            if isinstance(layer, layers.Dropout):
+                x = layer(x, training=training)
+            else:
+                x = layer(x)
+        return x
+
+class ModelVoice_v2(tf.keras.Model):
+    def __init__(self, output_units:int=31, **kwargs):
+        super(ModelVoice_v2, self).__init__(**kwargs)
+        #self.input_layer = layers.Input(shape=input_shape) # inferred automatically
+        self.layers_list = [
+            layers.Conv2D(64, (7, 3), activation='relu', padding='same', name='Conv2D_1'),
+            layers.MaxPooling2D(pool_size=(1, 3), name='MaxPool2D_1'),
+            layers.Conv2D(128, (1, 7), activation='relu', padding="same", name='Conv2D_2'),
+            layers.MaxPooling2D(pool_size=(1, 4), name='MaxPool2D_2'),
+            layers.Conv2D(256, (1, 10), activation='relu', padding="valid", name='Conv2D_3'),
+            layers.Conv2D(512, (7, 1), activation='relu', padding="valid", name='Conv2D_4'),
+            layers.GlobalMaxPooling2D(),
+            layers.Dropout(0.5, name='Dropout_1'),
+            layers.Dense(256, name='Dense_1'),
+            layers.Dense(output_units, name='Output')
+        ]
+    
+    def call(self, inputs, training=False):
+        x = inputs
+        #x = self.input_layer(inputs)
+        for layer in self.layers_list:
+            if isinstance(layer, layers.Dropout):
+                x = layer(x, training=training)
+            else:
+                x = layer(x)
+        return x
+    
+
 
 # class ModelVoice_2(tf.keras.Model):
 #     def __init__(self, data, **kwargs):
